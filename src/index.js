75d2b01a886f2862584e9635284912322e1f6f1f
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2020-05-11 20:03:29 +0200

    Gra gotowa z historią

--- src/index.js
+++ src/index.js
@@ -56,11 +56,12 @@ class Game extends React.Component {
                 squares: Array(9).fill(null),
             }],
             xIsNext: true,
+            stepNumber: 0
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         const winner = calculateWinner(current.squares);
@@ -71,14 +72,22 @@ class Game extends React.Component {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{ squares: squares }]),
-            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
         });
     }
 
+    jumpTo(step) {
+        this.setState(
+            {
+                xIsNext: (step % 2) === 0,
+                stepNumber: step
+            });
+    }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -86,7 +95,7 @@ class Game extends React.Component {
                 'Go to move #' + move :
                 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>)
         });
@@ -113,10 +122,6 @@ class Game extends React.Component {
                 </div>
             </div>
         );
-    }
-
-    jumpTo() {
-        alert("nie zaimplementowano");
     }
 }
 
